3c30ee3780dbaa6ae94f59df2b7d1c1bade094cd
Danny Weitekamp <dannyweitekamp@gmail.com>
2023-12-06 14:00:42 -0500

    added model stats + figures

---
+++ encode_all_sim.sh
@@ -0,0 +1,37 @@
+# !/bin/zsh
+
+python3.10 encode.py --dataset assistments09 -sim -i 
+python3.10 encode.py --dataset assistments12 -sim -i
+python3.10 encode.py --dataset assistments15 -sim -i
+python3.10 encode.py --dataset assistments17 -sim -i
+python3.10 encode.py --dataset bridge_algebra06 -sim -i
+python3.10 encode.py --dataset algebra05 -sim -i
+python3.10 encode.py --dataset spanish -sim -i
+python3.10 encode.py --dataset statics -sim -i
+
+python3.10 encode.py --dataset assistments09 -sim -s -sc -w -a
+python3.10 encode.py --dataset assistments12 -sim -s -sc -w -a
+python3.10 encode.py --dataset assistments15 -sim -s -sc -w -a
+python3.10 encode.py --dataset assistments17 -sim -s -sc -w -a
+python3.10 encode.py --dataset bridge_algebra06 -sim -s -sc -w -a
+python3.10 encode.py --dataset algebra05 -sim -s -sc -w -a
+python3.10 encode.py --dataset spanish -sim -s -sc -w -a
+python3.10 encode.py --dataset statics -sim -s -sc -w -a
+
+python3.10 encode.py --dataset assistments09 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset assistments12 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset assistments15 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset assistments17 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset bridge_algebra06 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset algebra05 -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset spanish -sim -i -s -sc -w -a -tw
+python3.10 encode.py --dataset statics -sim -i -s -sc -w -a -tw
+
+python3.10 encode.py --dataset assistments09 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset assistments12 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset assistments15 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset assistments17 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset bridge_algebra06 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset algebra05 -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset spanish -sim -i -s -ic -sc -tc -w -a
+python3.10 encode.py --dataset statics -sim -i -s -ic -sc -tc -w -a
